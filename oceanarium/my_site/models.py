67711330b9374582edd4d5d94d8015a75010e9b4
erjan <johnydepp2002@mail.ru>
2020-11-09 19:32:03 +0600

    create models for Customer

--- oceanarium/my_site/models.py
+++ oceanarium/my_site/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
-# Create your models here.
 
+class Customer(models.Model):
+    full_name = models.CharField(max_length=100,null=True)
+    email = models.CharField(max_length=100,null=True)
+    address = models.CharField(max_length=100,null=True)
+    phone = models.CharField(max_length=100,null=True)
