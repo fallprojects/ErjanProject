2790b1dbfc4e78b6341165a2cef9a80e46e6be39
erjan <johnydepp2002@mail.ru>
2020-11-10 19:37:27 +0600

    added class Ticket in models.py

--- oceanarium/my_site/models.py
+++ oceanarium/my_site/models.py
@@ -6,3 +6,8 @@ class Customer(models.Model):
     email = models.CharField(max_length=100,null=True)
     address = models.CharField(max_length=100,null=True)
     phone = models.CharField(max_length=100,null=True)
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=100,null=True)
+    description = models.CharField(max_length=100,null=True)
+    price = models.CharField(max_length=100,null=True)
