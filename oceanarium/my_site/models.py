7a2f80729b2ace9f0447ef4c3091ffd724b35903
erjan <johnydepp2002@mail.ru>
2020-11-12 19:32:32 +0600

    add new parameters img and text in models.py

--- oceanarium/my_site/models.py
+++ oceanarium/my_site/models.py
@@ -1,37 +1,36 @@
 from django.db import models
 
 class Customer(models.Model):
-    full_name = models.CharField(max_length=100,null=True)
-    email = models.CharField(max_length=100,null=True)
-    address = models.CharField(max_length=100,null=True)
-    phone = models.CharField(max_length=100,null=True)
+    full_name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.full_name
 
 class Ticket(models.Model):
-    name = models.CharField(max_length=100,null=True)
-    description = models.CharField(max_length=100,null=True)
-    price = models.CharField(max_length=100,null=True)
+    name = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=100, null=True)
+    price = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
 class Order(models.Model):
     Status =(
-        ('Sales','Sales'),
-        ('Sell out','Sell out')
+        ('Sales', 'Sales'),
+        ('Sell out', 'Sell out')
     )
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-    ticket = models.ForeignKey(Ticket,null=True,on_delete=models.SET_NULL)
+    ticket = models.ForeignKey(Ticket, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=100, null=True, choices=Status)
 
-    def __str__(self):
-        return self.ticket.name
-
 class Type(models.Model):
-    name = models.CharField(max_length=100,null=True)
+    name = models.CharField(max_length=100, null=True)
     description = models.CharField(max_length=100, null=True)
+    img = models.ImageField(null=True,blank=True)
+    text = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.name
