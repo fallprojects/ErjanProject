ed8fda254b4e3971cc5e55c877bfa4a91b3743fa
erjan <johnydepp2002@mail.ru>
2020-11-16 16:12:49 +0600

    add registration, authorization, commits

--- oceanarium/my_site/views.py
+++ oceanarium/my_site/views.py
@@ -2,6 +2,36 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from .form import Orderform
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,authenticate,logout
+from .form import *
+
+
+def registration(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request,'my_site/registration.html', context)
+
+def authorization(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        if user is not None:
+            return redirect('home')
+    context = {}
+    return render(request, 'my_site/authorization.html', context)
+
+def logout_page(reguest):
+    logout(reguest)
+    return redirect('home')
+
 
 def home_page(request):
     customers = Customer.objects.all()
@@ -41,13 +71,22 @@ def type_id(request,pk):
     context = {'type_ids':type_ids}
     return render(request,'my_site/type_id.html',context)
 
+def commit(reguest):
+    commits = Commit.objects.all()
+    form = Commit()
+    if reguest.method == 'POST':
+        form = Commitform(reguest.POST)
+        if form.is_valid:
+            form.save()
+    context = {'commits':commits,'form':form}
+    return render(reguest,'my_site/comments.html', context)
+
 def about(request):
     cus = Customer.objects.all()
     context = {'cus':cus}
-    return render(request, 'my_site/about.html', context)
+    return render(request,'my_site/about.html', context)
 
 def contacts(request):
     cus = Customer.objects.all()
     context = {'cus':cus}
     return render(request, 'my_site/contacts.html', context)
-
