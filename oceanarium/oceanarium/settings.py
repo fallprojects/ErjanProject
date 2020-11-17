b2f389e18613a274bc5cb408f539aa2918a2ecf4
erjan <johnydepp2002@mail.ru>
2020-11-17 18:42:36 +0600

    import include

--- oceanarium/oceanarium/settings.py
+++ oceanarium/oceanarium/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+from django.conf.urls import include
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'my_site',
+
 ]
 
 MIDDLEWARE = [
