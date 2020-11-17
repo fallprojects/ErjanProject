669d8a8b7be10df2217c0ef142a14adb5e703fb5
erjan <johnydepp2002@mail.ru>
2020-11-17 18:43:11 +0600

    add link include

--- oceanarium/oceanarium/urls.py
+++ oceanarium/oceanarium/urls.py
@@ -21,6 +21,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('my_site.urls'))
+    path('',include('my_site.urls')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
