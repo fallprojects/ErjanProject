537313b59948aebda41ca89f0759afc14b0ed1fa
erjan <johnydepp2002@mail.ru>
2020-11-17 18:36:22 +0600

    add table for decription

--- oceanarium/my_site/admin.py
+++ oceanarium/my_site/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
 from  .models import  *
-admin.site.register([Customer,Ticket,Order,Type,Commit])
+admin.site.register([Customer,Ticket,Order,Type,Commit,About,Contacts])
